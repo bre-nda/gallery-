06658a14cf5d0891476cf5ebd8b2a852066c17fb
bre-nda <brenda.kanale@student.moringaschool.com>
2021-11-27 16:38:56 +0300

    update our views function

--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Category, Location
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'welcome.html')
+    category = Category.objects.all()
+    location = Location.objects.all()
+
+    return render(request, 'index.html', {"location": location,"category":category})
 
 
