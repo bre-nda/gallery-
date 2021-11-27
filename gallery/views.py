39489af8eacfdbc322d6fad5183aad09309ac7b2
bre-nda <brenda.kanale@student.moringaschool.com>
2021-11-27 15:32:36 +0300

    create the index.css file

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-import datetime as dt
+
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
