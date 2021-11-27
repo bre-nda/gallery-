810de71760ad4123ab710a9a7862623818f1229b
bre-nda <brenda.kanale@student.moringaschool.com>
2021-11-27 15:26:41 +0300

    run tests for location and category

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Location, Category
 
 # Create your tests here.
+class LocationTestCase(TestCase):
+    def setUp(self):
+        self.name = Location(name='London')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.name,Location))
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        self.name = Category(name='Beach')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.name,Category))
