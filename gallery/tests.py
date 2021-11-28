7506ab9787bcc013c51117df1c41f6c982332e95
bre-nda <brenda.kanale@student.moringaschool.com>
2021-11-28 11:12:49 +0300

    run the tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Location, Category
+from .models import Location, Category, Image
 
 # Create your tests here.
 class LocationTestCase(TestCase):
@@ -15,3 +15,40 @@ class CategoryTestCase(TestCase):
         
     def test_instance(self):
         self.assertTrue(isinstance(self.name,Category))
+
+class ImageTestClass(TestCase):
+
+    #set up method
+    def setUp(self):
+        self.location = Location(name='Nairobi')
+        self.location.save()
+
+        self.category = Category(name='park')
+        self.category.save()
+
+        self.imagee = Image(id=1, image='image.jpg', description='this is a park', location=self.location,category=self.category)
+    # Testing instance
+    def test_instace(self):
+        self.assertTrue(isinstance(self.imagee,Image))
+
+    def test_save_image(self):
+        self.imagee.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_image(self):
+        self.imagee.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+    
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
+    def test_update_image(self):
+        self.imagee.save_image()
+        self.imagee.update_image(self.imagee.id, 'photos/test.jpg')
+        changed_img = Image.objects.filter(image='photos/test.jpg')
+        self.assertTrue(len(changed_img) > 0)
+
