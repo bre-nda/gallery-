4c01c015d4859a7ac7fb551f0a695327a66e118b
bre-nda <brenda.kanale@student.moringaschool.com>
2021-11-28 11:09:36 +0300

    add image model to the admin file

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Location, Category
+from .models import Location, Category, Image
 
 admin.site.register(Location)
 admin.site.register(Category)
+admin.site.register(Image)
 
 
 # Register your models here.
